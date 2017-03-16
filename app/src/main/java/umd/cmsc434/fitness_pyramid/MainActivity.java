2bd62ff71b032fa780adfdc22ca6b3c6c17d3c97
aibrahi <aibrahi1@umd.edu>
2017-03-16 15:45:33 -0400

    Initial commit

---
+++ app/src/main/java/umd/cmsc434/fitness_pyramid/MainActivity.java
@@ -0,0 +1,13 @@
+package umd.cmsc434.fitness_pyramid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
